ab794a6722a4de429a75f87df5f2c81e98a2f106
Matt Baker <matthew.baker4175@gmail.com>
2023-12-13 23:00:56 -0500

    finish day 8

--- Day8 - Part 1/Program.cs
+++ Day8 - Part 1/Program.cs
@@ -1,4 +1,6 @@
-﻿//var inputs = new string[]
+﻿using MathNet.Numerics;
+
+//var inputs = new string[]
 //{
 //    "LLR",
 //    "",
@@ -7,9 +9,25 @@
 //    "ZZZ = (ZZZ, ZZZ)"
 //};
 
+
+//var inputs = new string[]
+//{
+//    "LR",
+//    "",
+//    "11A = (11B, XXX)",
+//    "11B = (XXX, 11Z)",
+//    "11Z = (11B, XXX)",
+//    "22A = (22B, XXX)",
+//    "22B = (22C, 22C)",
+//    "22C = (22Z, 22Z)",
+//    "22Z = (22B, 22B)",
+//    "XXX = (XXX, XXX)"
+//};
+
 var inputs = File.ReadAllLines("values.txt");
 
-PartOne(inputs);
+//PartOne(inputs);
+PartTwo(inputs);
 void PartOne(string[] inputs)
 {
     var directionLine = inputs[0];
@@ -46,7 +64,58 @@ void PartOne(string[] inputs)
     }
 
     Console.WriteLine("Step Count: " + stepCount);
+}
 
+void PartTwo(string[] inputs)
+{
+    var directionLine = inputs[0];
+    var directionQueue = new Queue<char>();
+
+    var nodeLines = inputs.Skip(2).Take(inputs.Length).ToArray();
+    var mapping = new Dictionary<string, Node>();
+    for (int i = 0; i < nodeLines.Length; i++)
+    {
+        string key = nodeLines[i].Substring(0, 3);
+        string left = nodeLines[i].Substring(7, 3);
+        string right = nodeLines[i].Substring(12, 3);
+        var node = new Node(key, left, right);
+        mapping.Add(key, node);
+    }
+
+    var currentNodes = mapping.Values.Where(x => x.Key.EndsWith('A'));
+    var targetNodes = mapping.Values.Where(x => x.Key.EndsWith('Z'));
+
+    var cycleCounts = new Dictionary<string, long>();
+    foreach (var node in currentNodes)
+    {
+        cycleCounts.Add(node.Key, 0);
+        long stepCount = 0;
+        var currentNode = node;
+        // so we basically want to count how long it takes for 
+        // each individual node to get to its target node.
+        // we can then use the LCM of all of those values to
+        // determine how long it will take for all of them to   
+        // reach their target nodes at the same time
+        while (!currentNode.Key.EndsWith('Z'))
+        {
+            if (directionQueue.Count == 0)
+                foreach (char d in directionLine)
+                    directionQueue.Enqueue(d);
+
+            char direction = directionQueue.Dequeue();
+            if (direction == 'R')
+                currentNode = mapping[currentNode.Right];
+            else
+                currentNode = mapping[currentNode.Left];
+
+            stepCount++;
+        }
+        cycleCounts[node.Key] = stepCount;
+    }
+
+    // ok ok, so I know I just searched for a nuget package to do the
+    // LCM for me...sorry...I'm too buzzed to think that out at the moment
+    Console.WriteLine(Euclid.LeastCommonMultiple(cycleCounts.Values.ToArray()));
 }
 
 class Node

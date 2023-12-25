b3ab770b4ba60b146245930721c6febffe7fba47
Matt Baker <matthew.baker4175@gmail.com>
2023-12-24 20:51:52 -0500

    sigh, duplicate numbers in same row really screwed this up for me

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,52 +1,119 @@
-﻿var input = new List<string> {
-    "467..114..",
-    "...*......",
-    "..35..633.",
-    "......#...",
-    "617*......",
-    ".....+.58.",
-    "..592.....",
-    "......755.",
-    "...$.*....",
-    ".664.598.."
-};
-var parts = new List<PartNumber>();
-var symbols = new List<Symbol>();
-for (int i = 0; i < input.Count; i++)
-{
-    var tokens = input[i].Split('.')
-        .Where(x => !string.IsNullOrEmpty(x));
+﻿bool useTest = false;
+string fileName = useTest ? "sample.txt" : "input.txt";
 
-    if (tokens.Any())
+var input = File.ReadAllLines(fileName);
+PartOne(input);
+//between 535294 and 535353
+void PartOne(string[] input)
+{
+    char[,] grid = new char[input.Length, input[0].Length];
+    for (int row = 0; row < input.Length; row++)
     {
-        foreach (var token in tokens)
+        for (int col = 0; col < input[row].Length; col++)
         {
-            if (int.TryParse(token, out var value))
+            grid[row, col] = input[row][col];
+        }
+    }
+
+    Print(grid);
+    var seenParts = new List<int>();
+    var parts = new List<int>();
+    for (int row = 0; row < input.Length; row++)
+    {
+        for (int col = 0; col < input[row].Length; col++)
+        {
+            string number = string.Empty;
+            if (char.IsNumber(grid[row, col]))
             {
+                int startIndex = col;
+                int endIndex = col;
+                number += grid[row, col];
+                while (col < input[row].Length - 1)
+                {
+                    col++;
+                    if (!char.IsNumber(grid[row, col]))
+                        break;
+
+                    number += grid[row, col];
+                    endIndex = col;
+                }
+
                 var partNum = new PartNumber()
                 {
-                    Row = i,
-                    StartColumn = input[i].IndexOf(token),
-                    EndColumn = input[i].IndexOf(token) + token.Length,
-                    Value = token
+                    Row = row,
+                    StartColumn = startIndex,
+                    EndColumn = endIndex,
+                    Value = int.Parse(number)
                 };
-                parts.Add(partNum);
-
-            } 
-            else
-            {
-                var symbol = new Symbol()
-                {
-                    Row = i,
-                    Column = input[i].IndexOf(token),
-                    Value = token
-                };
-                symbols.Add(symbol);
+                if (IsValidPartNumber(partNum, grid))
+                    parts.Add(partNum.Value);
             }
         }
     }
+
+
+    foreach (var part in parts.OrderBy(p => p))
+        Console.WriteLine(part);
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Sum of parts: " + parts.Sum());// /*parts.Sum(p => p.Value)*/);
 }
 
+bool IsValidPartNumber(PartNumber partNumber, char[,] grid)
+{
+    int leftColIndex = partNumber.StartColumn - 1;
+    int rightColIndex = partNumber.EndColumn + 1;
+    if (leftColIndex > 0)
+        if (grid[partNumber.Row, leftColIndex] != '.' && !char.IsNumber(grid[partNumber.Row, leftColIndex]))
+            return true;
+    if (rightColIndex < grid.GetLength(1))
+        if (grid[partNumber.Row, rightColIndex] != '.' && !char.IsNumber(grid[partNumber.Row, rightColIndex]))
+            return true;
+
+    int topRow = partNumber.Row - 1;
+    if (topRow >= 0)
+    {
+        for (int col = leftColIndex; col <= rightColIndex; col++)
+        {
+            if (col < 0 || col >= grid.GetLength(1))
+                continue;
+           
+            if (grid[topRow, col] != '.' && !char.IsNumber(grid[topRow, col]))
+                return true;
+        }
+    }
+
+    int bottomRow = partNumber.Row + 1;
+    if (bottomRow < grid.GetLength(0))
+    {
+        for (int col = leftColIndex; col <= rightColIndex; col++)
+        {
+            if (col < 0 || col >= grid.GetLength(1))
+                continue;
+           
+            if (grid[bottomRow, col] != '.' && !char.IsNumber(grid[bottomRow, col ]))
+                return true;
+        }
+    }
+
+    return false;
+}
+
+void Print(char[,] grid)
+{
+    for (int row = 0; row < grid.GetLength(0); row++)
+    {
+        for (int col = 0; col < grid.GetLength(1); col++)
+        {
+            Console.Write(grid[row, col]);
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
 
 class Symbol
 {
@@ -60,5 +127,5 @@ class PartNumber
     public int Row { get; set; }
     public int StartColumn { get; set; }
     public int EndColumn { get; set; }
-    public string Value { get; set; }
+    public int Value { get; set; }
 }
